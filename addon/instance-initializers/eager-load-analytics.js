8e6cec699cf52fb1b48d3f72dc173871bfc83303
Brian Whitton <me@brianwhitton.com>
2019-05-08 14:53:37 -0400

    make this eager-load opt-outable

--- addon/instance-initializers/eager-load-analytics.js
+++ addon/instance-initializers/eager-load-analytics.js
@@ -1,7 +1,11 @@
+import config from 'ember-get-config';
 // We want this service to start listening immediately without
 // needing the consumer to inject it somewhere
 
 export function initialize(appInstance) {
+  if (config['nypr-metrics'] && config['nypr-metrics'].disableEagerListenAnalytics) {
+    return;
+  }
   appInstance.inject('controller:application', '_listenAnalytics', 'service:listen-analytics');
 
   // We don't just want to look it up (like so: appInstance.lookup('service:listen-analytics'))
